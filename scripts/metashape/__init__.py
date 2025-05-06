77ea57a4089b0fd7faedbe568a34105d6a523249
JuancarlosMH <jcmontesh@gmail.com>
2025-05-06 16:12:23 +1000

    fix-name-import

--- scripts/metashape/__init__.py
+++ scripts/metashape/__init__.py
@@ -1,3 +1,11 @@
 """
 TERN Dronescape Metashape Processing Package
-""" 
+"""
+
+# Import all necessary functions to expose them at the package level
+from .image_utils import find_filtered_images, filter_images_by_timestamp, filter_multispec_by_flight_pattern
+from .gpu_setup import setup_gpu
+from .utils import find_images
+from .camera_ops import configure_multispectral_camera
+from .processing import detect_reflectance_panels, merge_chunks
+from .markers import find_marker_files, load_markers 
